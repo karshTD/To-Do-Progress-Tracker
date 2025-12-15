a41213f7ade390e7b61f4957ba98b56853ac5610
Utkarsh <132635254+karshTD@users.noreply.github.com>
2025-12-15 09:38:39 +0530

    Create graph.hpp

---
+++ src folder/graph.hpp
@@ -0,0 +1,14 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <vector>
+#include <map>
+#include <string>
+#include "tasks.hpp"
+
+class Graph {
+public:
+    void showProgress(const std::vector<Task>& tasks);
+};
+
+#endif
